9057b4037c439c787da2bff7080c566549a6a86f
vmt <vmttq311@gmail.com>
2024-04-03 13:47:13 +0300

    fix

--- lab2/fclient/app/src/main/java/com/example/fclient/MainActivity.java
+++ lab2/fclient/app/src/main/java/com/example/fclient/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
 import com.example.fclient.databinding.ActivityMainBinding;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'fclient' library on application startup.
@@ -16,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("fclient");
         System.loadLibrary("mbedcrypto");
     }
-
+    ActivityResultLauncher activityResultLauncher;
     private ActivityMainBinding binding;
 
     @Override
@@ -33,20 +43,27 @@ public class MainActivity extends AppCompatActivity {
         int res = initRng();
         byte[] v = randomBytes(10);
 
-        byte[] key = new byte[16];
-        byte[] data = new byte[32];
-        byte[] encrypted = encrypt(key, data);
-        byte[] decrypted = decrypt(key, data);
-        System.out.println(Arrays.toString(encrypted));
-        System.out.println(Arrays.toString(decrypted));
-        System.out.println(res);
-        System.out.println(Arrays.toString(v));
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                (ActivityResultCallback<ActivityResult>) result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // обработка результата
+                        assert data != null;
+                        String pin = data.getStringExtra("pin");
+                        if (pin.length() == 4) Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        else {
+                            Toast.makeText(MainActivity.this, "Please enter valid pin code", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-
-
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
+    }
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
     }
 
     /**
@@ -58,7 +75,19 @@ public class MainActivity extends AppCompatActivity {
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
-
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
 
 
 
