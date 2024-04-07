70d1a2f5f90735caa632c364b7ca5506ad3210e8
vmt <vmttq311@gmail.com>
2024-04-07 22:36:55 +0300

    lab5

---
+++ lab5/backend/src/main/java/com/example/backend/models/Country.java
@@ -0,0 +1,22 @@
+package com.example.backend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "countries")
+@Access(AccessType.FIELD)
+public class Country {
+
+    public Country() { }
+    public Country(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    public long id;
+
+    @Column(name = "name")
+    public String name;
+}
