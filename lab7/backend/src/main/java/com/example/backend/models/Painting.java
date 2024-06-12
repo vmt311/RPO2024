cc5a8f37677e7cf40d186ed6c8e43ec5ff929201
vmt311 <vmttq311@gmail.com>
2024-06-13 00:36:59 +0300

    fix

--- lab7/backend/src/main/java/com/example/backend/models/Painting.java
+++ lab7/backend/src/main/java/com/example/backend/models/Painting.java
@@ -1,7 +1,7 @@
 package com.example.backend.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "paintings")
